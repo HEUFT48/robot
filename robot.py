b99b42514f7d4ef7eff617ddfd0c285e16e302ed
HEUFT48 <1911colt1911@gmail.com>
2025-06-16 13:39:32 +0300

    rob_new

--- robot.py
+++ robot.py
@@ -1,14 +1,13 @@
-from typing import List
-
-
-def platforms(robots: List[int], limit: int) -> int:
+# id 139338173 решения на яндекс контест.
+def platforms(robots: list[int], limit: int) -> int:
     """Метод вычисления кол-во платформ"""
-    robots.sort()
-    left, right = 0, len(robots) - 1
+    sorted_robots: int = sorted(robots)
+    left: int = 0
+    right: int = len(sorted_robots) - 1
     platforms = 0
 
     while left <= right:
-        if robots[left] + robots[right] <= limit:
+        if sorted_robots[left] + sorted_robots[right] <= limit:
             left += 1
         right -= 1
         platforms += 1
